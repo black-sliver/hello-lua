c7f72a370cb8b8ed0d5ea84defc65caae1c7ec39
black-sliver <59490463+black-sliver@users.noreply.github.com>
2023-04-09 22:18:19 +0200

    fix link order

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@
 #gcc -llua main.c -o hello-lua  # for arch linux
 
 # Ubuntu:
-gcc -I/usr/include/lua5.4 -llua5.4 main.c -o hello-lua  # for ubuntu
+gcc -I/usr/include/lua5.4 main.c -llua5.4 -ldl -o hello-lua  # for ubuntu
 
